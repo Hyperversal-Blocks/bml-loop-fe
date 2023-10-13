719d9aee8cd2b211b690ba3d2d1fd36584d59245
Haseeb Raja <has33braja@gmail.com>
2023-10-13 16:33:40 +0500

    feat: added on mount and on disconnect feature for metamask

--- src/composables/useWallet.js
+++ src/composables/useWallet.js
@@ -1,16 +1,17 @@
 // useMetamask.js
-import { ref } from 'vue';
+import { ref, watch, onMounted, onUnmounted } from 'vue';
 import Web3 from 'web3';
 
 export default function useMetamask() {
     const account = ref(null);
     const ethEnabled = ref(false);
+    let poller = null;
 
     const connectMetamask = async () => {
         if (window.ethereum) {
             window.web3 = new Web3(window.ethereum);
             try {
-                await window.ethereum.request({method: 'eth_requestAccounts'});
+                await window.ethereum.request({ method: 'eth_requestAccounts' });
                 account.value = (await window.web3.eth.getAccounts())[0];
                 ethEnabled.value = true;
             } catch (e) {
@@ -20,6 +21,27 @@ export default function useMetamask() {
             console.error("No web3? Consider trying MetaMask!");
         }
     };
+
+    const checkAccountChange = async () => {
+        const accounts = await window.web3?.eth.getAccounts();
+        if (account.value !== accounts[0]) {
+            account.value = accounts[0] || null;
+            ethEnabled.value = !!account.value;
+        }
+    };
+
+    onMounted(() => {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            poller = setInterval(checkAccountChange, 1000);
+        }
+    });
+
+    onUnmounted(() => {
+        if (poller) {
+            clearInterval(poller);
+        }
+    });
 
     return {
         account,
